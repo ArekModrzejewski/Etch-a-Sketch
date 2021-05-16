0670d6b5008b5ea07ce67be8f41a297ef180b74e
ArekModrzejewski <arekmod98@gmail.com>
2021-05-16 11:28:58 +0200

    first final version

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const divs = document.getElementsByClassName('neutral-div');
+const reset_button = document.getElementById('reset-button');
+const size_button = document.getElementById('size-button');
+
 //funtion creating divs taking number of columns of the square grid
 function createDivs(numberOfColumns) {
     let i = 0;
@@ -6,13 +10,44 @@ function createDivs(numberOfColumns) {
         const content = document.createElement('div');
         content.classList.add('neutral-div')
         container.appendChild(content);
+        container.style.gridTemplateColumns = `repeat(${numberOfColumns}, 1fr)`;
         i++;
     }
 }
-createDivs(16);
-const divs = document.getElementsByClassName('neutral-div');
-for (i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('mouseover', function (e) {
-        (e.target).classList.add('hovered-div');
+function removeDivs() {
+    document.querySelectorAll('.neutral-div').forEach(e => e.remove());
+}
+
+
+function drawingHover() {
+    for (i = 0; i < divs.length; i++) {
+        divs[i].addEventListener('mouseover', function (e) {
+            (e.target).classList.add('hovered-div');
+        }
+        )
     }
-)};
+}
+
+function resetColor() {
+    for (i = 0; i < divs.length; i++) {
+        divs[i].classList.remove('hovered-div')
+    }
+}
+function size_buttonFunction() {
+    let columns = window.prompt('Enter a number from range <4, 70>', 'Enter number here...')
+    if (columns >= 4 && columns <= 70) {
+        removeDivs()
+        createDivs(columns)
+        i++
+    }
+    else {
+        alert('Wrong input. Try again.')
+        size_buttonFunction()
+    }
+}
+reset_button.onclick = resetColor;
+
+size_button.onclick = size_buttonFunction;
+
+window.onmousedown = drawingHover;
+createDivs(16);
